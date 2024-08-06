af35f5d08e5f3c16fe5ffd35f3238937c3ec6ff1
NeilRow56 <neilrowland56@gmail.com>
2024-08-06 05:06:24 +0100

    Added Orders page with Table - 1st stage

--- src/app/dashboard/orders/page.tsx
+++ src/app/dashboard/orders/page.tsx
@@ -1,5 +1,58 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import React from "react";
 
 export default function OrdersPage() {
-  return <div>Orders Page</div>;
+  return (
+    <Card>
+      <CardHeader className="px-7">
+        <CardTitle>Orders</CardTitle>
+        <CardDescription>Recent orders from your store!</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Customer</TableHead>
+              <TableHead>Type</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Date</TableHead>
+              <TableHead className="text-right">Amount</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell>
+                <p className="font-medium">Ben</p>
+                <p className="hidden md:flex text-sm text-muted-foreground">
+                  ben@bt.com
+                </p>
+              </TableCell>
+              <TableCell>Order</TableCell>
+              <TableCell>pending</TableCell>
+              <TableCell>
+                {new Intl.DateTimeFormat("en-GB").format(27 / 5 / 2024)}
+              </TableCell>
+              <TableCell className="text-right">
+                £{new Intl.NumberFormat("en-GB").format(25040)}
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </CardContent>
+    </Card>
+  );
 }
--- src/components/dashboard/RecentSales.tsx
+++ src/components/dashboard/RecentSales.tsx
@@ -23,6 +23,36 @@ export function RecentSales() {
           </p>
         </div>
       </CardContent>
+      <CardContent className="flex flex-col gap-8">
+        <div className="flex items-center gap-4">
+          <Avatar className="hidden sm:flex h-9 w-9">
+            <AvatarImage src="/men.jpeg" alt="Avatar Image" />
+            <AvatarFallback>Ben</AvatarFallback>
+          </Avatar>
+          <div className="grid gap-1">
+            <p className="text-sm font-medium">Ben</p>
+            <p className="text-sm text-muted-foreground">ben@bt.com</p>
+          </div>
+          <p className="ml-auto font-medium">
+            +${new Intl.NumberFormat("en-US").format(25000 / 100)}
+          </p>
+        </div>
+      </CardContent>
+      <CardContent className="flex flex-col gap-8">
+        <div className="flex items-center gap-4">
+          <Avatar className="hidden sm:flex h-9 w-9">
+            <AvatarImage src="/men.jpeg" alt="Avatar Image" />
+            <AvatarFallback>Ben</AvatarFallback>
+          </Avatar>
+          <div className="grid gap-1">
+            <p className="text-sm font-medium">Ben</p>
+            <p className="text-sm text-muted-foreground">ben@bt.com</p>
+          </div>
+          <p className="ml-auto font-medium">
+            +${new Intl.NumberFormat("en-US").format(25000 / 100)}
+          </p>
+        </div>
+      </CardContent>
     </Card>
   );
 }
