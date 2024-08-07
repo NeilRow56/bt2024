db1f0261afa7aea0b8ea1a335e1b6a645b72e22e
NeilRow56 <neilrowland56@gmail.com>
2024-08-07 09:38:04 +0100

    Fetched product data and displated in product Table

--- src/app/dashboard/products/page.tsx
+++ src/app/dashboard/products/page.tsx
@@ -26,8 +26,22 @@ import {
 import { MoreHorizontal, PlusCircle, UserIcon } from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
+import db from "@/lib/db";
+import { unstable_noStore as noStore } from "next/cache";
 
-export default function ProductsPage() {
+async function getData() {
+  const data = await db.product.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return data;
+}
+
+export default async function ProductsPage() {
+  noStore();
+  const data = await getData();
   return (
     <>
       <div className="flex items-center justify-end">
@@ -52,50 +66,54 @@ export default function ProductsPage() {
                 <TableHead>Image</TableHead>
                 <TableHead>Name</TableHead>
                 <TableHead>Status</TableHead>
-                <TableHead>Price</TableHead>
-                <TableHead>Date</TableHead>
+                <TableHead className="text-right">Price (£)</TableHead>
+                <TableHead className="text-right">Date</TableHead>
                 <TableHead className="text-end">Actions</TableHead>
               </TableRow>
             </TableHeader>
             <TableBody>
-              <TableRow>
-                <TableCell>
-                  <Image
-                    alt="Product Image"
-                    src="/shoes/airmax-plus/pic-1.png"
-                    height={64}
-                    width={64}
-                    className="rounded-md object-cover h-16 w-16"
-                  />
-                </TableCell>
-                <TableCell>Air Max</TableCell>
-                <TableCell>In stock</TableCell>
-                <TableCell>£99.00</TableCell>
-                <TableCell>
-                  {new Intl.DateTimeFormat("en-GB").format(27.2)}
-                </TableCell>
-                <TableCell className="text-end">
-                  <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                      <Button size="icon" variant="ghost">
-                        <MoreHorizontal className="h-4 w-4" />
-                      </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end">
-                      <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                      <DropdownMenuSeparator />
-                      <DropdownMenuItem asChild>
-                        <Link href={`/dashboard/products/1`}>Edit</Link>
-                      </DropdownMenuItem>
-                      <DropdownMenuItem asChild>
-                        <Link href={`/dashboard/products/1/delete`}>
-                          Delete
-                        </Link>
-                      </DropdownMenuItem>
-                    </DropdownMenuContent>
-                  </DropdownMenu>
-                </TableCell>
-              </TableRow>
+              {data.map((item) => (
+                <TableRow key={item.id}>
+                  <TableCell>
+                    <Image
+                      alt="Product Image"
+                      src={item.images[0]}
+                      height={64}
+                      width={64}
+                      className="rounded-md object-cover h-16 w-16"
+                    />
+                  </TableCell>
+                  <TableCell>{item.name}</TableCell>
+                  <TableCell>{item.status}</TableCell>
+                  <TableCell className="text-right ">{item.price}</TableCell>
+                  <TableCell className="text-right">
+                    {new Intl.DateTimeFormat("en-GB").format(item.createdAt)}
+                  </TableCell>
+                  <TableCell className="text-end">
+                    <DropdownMenu>
+                      <DropdownMenuTrigger asChild>
+                        <Button size="icon" variant="ghost">
+                          <MoreHorizontal className="h-4 w-4" />
+                        </Button>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent align="end">
+                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem asChild>
+                          <Link href={`/dashboard/products/${item.id}`}>
+                            Edit
+                          </Link>
+                        </DropdownMenuItem>
+                        <DropdownMenuItem asChild>
+                          <Link href={`/dashboard/products/${item.id}/delete`}>
+                            Delete
+                          </Link>
+                        </DropdownMenuItem>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </TableCell>
+                </TableRow>
+              ))}
             </TableBody>
           </Table>
         </CardContent>
